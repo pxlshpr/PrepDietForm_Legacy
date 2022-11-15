a43a7889df491578e0bd255707497c177817d0e7
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-15 14:08:48 +0500

    tag increment

--- Sources/PrepDietForm/Views/TDEEForm/TDEEForm.swift
+++ Sources/PrepDietForm/Views/TDEEForm/TDEEForm.swift
@@ -78,8 +78,8 @@ struct TDEEForm: View {
     var navigationView: some View {
         NavigationStack(path: $path) {
 //            legacyForm
-//            formHealth
             formFormula
+//            formHealth
             .scrollDismissesKeyboard(.immediately)
             .navigationTitle("Maintenance Energy")
             .navigationBarTitleDisplayMode(.inline)
@@ -216,7 +216,6 @@ struct TDEEForm: View {
             }
             .padding(.horizontal, 17)
             .padding(.vertical, 15)
-            .background(Color(.secondarySystemFill))
         }
         
         var formulaRow: some View {
@@ -328,91 +327,230 @@ struct TDEEForm: View {
         }
     }
     
+    var formulaSectionNew: some View {
+        
+        
+        let useHealthAppDataBinding = Binding<Bool>(
+            get: { useHealthAppData },
+            set: { newValue in
+                withAnimation {
+                    useHealthAppData = newValue
+                }
+            }
+        )
+        var useHealthAppToggle: some View {
+            Toggle(isOn: useHealthAppDataBinding) {
+                HStack {
+                    appleHealthSymbol
+                    Text("Sync with Health App")
+                }
+            }
+            .toggleStyle(.button)
+        }
+        
+        var topSection: some View {
+            HStack {
+                HStack(spacing: 5) {
+                    Image(systemName: "function")
+                        .foregroundColor(.secondary)
+                    Text("Calculated")
+                        .foregroundColor(.secondary)
+                    Image(systemName: "chevron.up.chevron.down")
+                        .foregroundColor(Color(.tertiaryLabel))
+                        .imageScale(.small)
+                }
+                Spacer()
+//                useHealthAppToggle
+            }
+            .padding(.horizontal, 17)
+        }
+        
+        var formulaRow: some View {
+            HStack {
+                HStack {
+                    Text("Using")
+                        .foregroundColor(.secondary)
+                    PickerLabel("Katch-McArdle")
+                    Text("equation")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(.top, 8)
+        }
+        
+        var flowView: some View {
+            func label(_ prefix: String, _ string: String) -> some View {
+                var backgroundColor: Color {
+                    return colorScheme == .light ? Color(hex: "e8e9ea") : Color(hex: "434447")
+                }
+                return PickerLabel(
+                    string,
+                    prefix: prefix,
+                    systemImage: useHealthAppData ? nil : "chevron.right",
+//                    imageColor: <#T##Color#>,
+                    backgroundColor:  useHealthAppData ? Color(.systemGroupedBackground) : backgroundColor,
+                    foregroundColor: useHealthAppData ? Color(.secondaryLabel) : Color.primary,
+                    prefixColor: useHealthAppData ? Color(.tertiaryLabel) : Color.secondary,
+//                    imageScale: <#T##Image.Scale#>,
+                    infiniteMaxHeight: false
+                )
+            }
+            
+            return FlowView(alignment: .center, spacing: 10, padding: 17) {
+                ZStack {
+                    Capsule(style: .continuous)
+                        .foregroundColor(Color(.clear))
+                    HStack(spacing: 5) {
+                        Text("with")
+                            .foregroundColor(Color(.tertiaryLabel))
+                    }
+                    .frame(height: 25)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 5)
+                }
+                .fixedSize(horizontal: true, vertical: true)
+                Menu {
+                    Picker(selection: .constant(true), label: EmptyView()) {
+                        Text("Male").tag(true)
+                        Text("Female").tag(false)
+                    }
+                } label: {
+                    label("sex", "male")
+                }
+                Button {
+                    path.append(.fatPercentageForm)
+                } label: {
+                    label("fat", "29 %")
+                }
+                Button {
+                    path.append(.weightForm)
+                } label: {
+                    label("weight", "93.55 kg")
+                }
+                Button {
+                    path.append(.heightForm)
+                } label: {
+                    label("height", "177 cm")
+                }
+            }
+            .padding(.bottom, 5)
+        }
+    
+        return FormStyledSection(header: restingHeader, horizontalPadding: 0) {
+            VStack {
+                topSection
+                formulaRow
+                Divider()
+                    .frame(width: 300)
+                    .padding(.vertical, 5)
+                flowView
+                useHealthAppToggle
+                    .padding(.bottom)
+                HStack {
+                    Spacer()
+                    Text("2,024")
+                        .font(.system(.title3, design: .rounded, weight: .semibold))
+                    Text("kcal")
+                        .foregroundColor(.secondary)
+                }
+                .padding(.trailing)
+            }
+        }
+    }
     var formFormula: some View {
         FormStyledScrollView {
-            formulaSection
+            formulaSectionNew
+            activeHealthSection
+            restingHealthSection
+        }
+    }
+    
+    var restingHealthSection: some View {
+        FormStyledSection(header: restingHeader) {
+            VStack(spacing: 5) {
+                HStack {
+                    HStack(spacing: 5) {
+                        appleHealthSymbol
+                        Text("Health App")
+                            .foregroundColor(.secondary)
+                        Image(systemName: "chevron.up.chevron.down")
+                            .foregroundColor(Color(.tertiaryLabel))
+                            .imageScale(.small)
+                    }
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text("Use")
+                            .foregroundColor(.secondary)
+                        PickerLabel("daily average of")
+                    }
+                    Spacer()
+                }
+                .padding(.top)
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text("The past")
+                            .foregroundColor(Color(.secondaryLabel))
+                        PickerLabel("2")
+                        PickerLabel("weeks")
+                    }
+                    Spacer()
+                }
+                .padding(.bottom)
+                HStack {
+                    Spacer()
+                    Text("2,024")
+                        .font(.system(.title3, design: .rounded, weight: .semibold))
+                    Text("kcal")
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+    }
+    
+    var activeHealthSection: some View {
+        FormStyledSection(header: activeHeader) {
+            VStack(spacing: 5) {
+                HStack {
+                    HStack(spacing: 5) {
+                        appleHealthSymbol
+                        Text("Health App")
+                            .foregroundColor(.secondary)
+                        Image(systemName: "chevron.up.chevron.down")
+                            .foregroundColor(Color(.tertiaryLabel))
+                            .imageScale(.small)
+                    }
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text("Use")
+                            .foregroundColor(.secondary)
+                        PickerLabel("previous day's value")
+                    }
+                    Spacer()
+                }
+                .padding(.top)
+                .padding(.bottom)
+                HStack {
+                    Spacer()
+                    Text("1,203")
+                        .font(.system(.title3, design: .rounded, weight: .semibold))
+                    Text("kcal")
+                        .foregroundColor(.secondary)
+                }
+            }
         }
     }
     
     var formHealth: some View {
         FormStyledScrollView {
-            FormStyledSection(header: restingHeader) {
-                VStack(spacing: 5) {
-                    HStack {
-                        HStack(spacing: 5) {
-                            appleHealthSymbol
-                            Text("Health App")
-                                .foregroundColor(.secondary)
-                            Image(systemName: "chevron.up.chevron.down")
-                                .foregroundColor(Color(.tertiaryLabel))
-                                .imageScale(.small)
-                        }
-                        Spacer()
-                    }
-                    HStack {
-                        Spacer()
-                        HStack {
-                            Text("Use")
-                                .foregroundColor(.secondary)
-                            PickerLabel("daily average of")
-                        }
-                        Spacer()
-                    }
-                    .padding(.top)
-                    HStack {
-                        Spacer()
-                        HStack {
-                            Text("The past")
-                                .foregroundColor(Color(.secondaryLabel))
-                            PickerLabel("2")
-                            PickerLabel("weeks")
-                        }
-                        Spacer()
-                    }
-                    .padding(.bottom)
-                    HStack {
-                        Spacer()
-                        Text("2,024")
-                            .font(.system(.title3, design: .rounded, weight: .semibold))
-                        Text("kcal")
-                            .foregroundColor(.secondary)
-                    }
-                }
-            }
-            
-            FormStyledSection(header: activeHeader) {
-                VStack(spacing: 5) {
-                    HStack {
-                        HStack(spacing: 5) {
-                            appleHealthSymbol
-                            Text("Health App")
-                                .foregroundColor(.secondary)
-                            Image(systemName: "chevron.up.chevron.down")
-                                .foregroundColor(Color(.tertiaryLabel))
-                                .imageScale(.small)
-                        }
-                        Spacer()
-                    }
-                    HStack {
-                        Spacer()
-                        HStack {
-                            Text("Use")
-                                .foregroundColor(.secondary)
-                            PickerLabel("previous day's value")
-                        }
-                        Spacer()
-                    }
-                    .padding(.top)
-                    .padding(.bottom)
-                    HStack {
-                        Spacer()
-                        Text("1,203")
-                            .font(.system(.title3, design: .rounded, weight: .semibold))
-                        Text("kcal")
-                            .foregroundColor(.secondary)
-                    }
-                }
-            }
+            restingHealthSection
+            activeHealthSection
         }
     }
 }
