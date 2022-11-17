31b677b4a6fbf925f40bed8d0f4ac95b2bb89de4
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-17 22:59:58 +0500

    tag increment

--- Sources/PrepDietForm/Views/LeanBodyMassForm.swift
+++ Sources/PrepDietForm/Views/LeanBodyMassForm.swift
@@ -58,7 +58,7 @@ struct LeanBodyMassForm: View {
             }
             return HStack {
                 HStack {
-                    Text("Using")
+                    Text("using")
                         .foregroundColor(Color(.tertiaryLabel))
                     menu
                     Text("formula")
